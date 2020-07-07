23875f2a6f23eba7c1099227e238a6e6f368c673
Karen Dolan <karen_dolan@harvard.edu>
2020-07-07 14:06:04 -0400

    lab3 test

---
+++ lab3/app.py
@@ -0,0 +1,53 @@
+# ----------------------
+# Karen Dolan lab3
+# CSCI S-14a 2020
+# ----------------------
+from flask import Flask, render_template, request, redirect, url_for
+from models.user import Db, User
+from modules.userform import UserForm
+
+app = Flask(__name__)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/usersdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = "s14a-key"
+Db.init_app(app)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    # Query all
+    users = User.query.all()
+
+    # Iterate and print
+    for user in users:
+        User.tostring(user)
+        return render_template("index.html")
+
+
+# @route /adduser - GET, POST
+@app.route('/adduser', methods=['GET', 'POST'])
+def addUser():
+    form = UserForm()
+    # If GET
+    if request.method == 'GET':
+        return render_template('adduser.html', form=form)
+    # If POST
+    else:
+        if form.validate_on_submit():
+            first_name = request.form['first_name']
+            age = request.form['age']
+            new_user = User(first_name=first_name, age=age)
+            Db.session.add(new_user)
+            Db.session.commit()
+            return redirect(url_for('index'))
+        else:
+            return render_template('adduser.html', form=form)
+
+
+# @route /adduser/<first_name>/<age>
+@app.route('/adduser/<first_name>/<age>')
+def addUserFromUrl(first_name, age):
+    Db.session.add(User(first_name=first_name, age=age))
+    Db.session.commit()
+    return redirect(url_for('index'))
