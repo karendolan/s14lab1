20a7b46e5a323f6cfa87184f3f179c6005eafc42
Karen Dolan <karen_dolan@harvard.edu>
2020-07-12 13:10:56 -0400

    HW3 random generator

--- lab3/app.py
+++ lab3/app.py
@@ -5,6 +5,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 from models.user import db, User
 from modules.userform import UserForm
+# https://pypi.org/project/names/
+import names
+import random
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/usersdb'
@@ -50,9 +53,38 @@ def addUserFromUrl(first_name, age):
     db.session.commit()
     return redirect(url_for('index'))
 
-    # @route /removeuser/<id>
-    @app.route('/removeuser/<id>')
-    def removeUserFromUrl(id):
+# @route /removeuser/<id>
+@app.route('/removeuser/<user_id>')
+def removeUserFromUrl(user_id):
+    # Query user
+    user = User.query.get_or_404(
+        user_id,
+        description='There is no User with user_id {}'.format(user_id)
+    )
+    # Delete user
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+# @route /adduser/<first_name>/<age>
+@app.route('/updateuser/<user_id>/<first_name>/<age>')
+def updateUserFromUrl(user_id, first_name, age):
+    db.session.query(User).\
+        filter(User.user_id==user_id).\
+        update({"age":age, "first_name": first_name})
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+# @route /adduser/<first_name>/<age>
+@app.route('/generateusers/<count>')
+def randomGenerateUsers(count):
+    if(not count.isnumeric()):
+        return redirect(url_for('index'))
+
+    for x in range(0, int(count)):
+        first_name = names.get_first_name()
+        age = random.randint(0, 140)
         db.session.add(User(first_name=first_name, age=age))
         db.session.commit()
-        return redirect(url_for('index'))
+    return redirect(url_for('index'))
